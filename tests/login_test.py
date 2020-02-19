3b90e095edf938d71b31c41bc0499aa959d6ccc5
erikb <erikburton1@gmail.com>
2020-02-19 06:41:32 -0700

    Creates new methods and imports page object. Updates setup fixture

--- tests/login_test.py
+++ tests/login_test.py
@@ -1,13 +1,12 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
+from pages import login_page
 import os
 
 class TestLogin():
 
     @pytest.fixture
-    def driver(self, request):
+    def login(self, request):
         _chromedriver = os.path.join(os.getcwd(), 'vendor', 'chromedriver')
         driver_ = webdriver.Chrome()
 
@@ -17,10 +16,6 @@ class TestLogin():
         request.addfinalizer(quit)
         return driver_
 
-    def test_valid_credentials(self, driver):
-        driver.get("https://the-internet.herokuapp.com/login")
-        driver.find_element(By.ID, "username").send_keys("tomsmith")
-        driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
-        driver.find_element(By.CSS_SELECTOR, "button").click()
-        time.sleep(1)
-        assert driver.find_element(By.CSS_SELECTOR, ".flash.success").is_displayed()
+    def test_valid_credentials(self, login):
+        login.with_("tomsmith", "SuperSecretPassword!")
+        assert login.success_message_present()
